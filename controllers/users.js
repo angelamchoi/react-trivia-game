d3283bd3e6edf05724aa5a7c842facd24f4bc538
Angela Choi <angelamchoi@gmail.com>
2021-07-29 16:33:59 -0400

    added wireframe pics

--- controllers/users.js
+++ controllers/users.js
@@ -1,15 +1,31 @@
 const User = require('../models/user');
+// const jwt = require("jsonwebtoken");
+// const SECRET = process.env.SECRET;
 
 async function signup(req,res) {
     const user = new User(req.body)
     try {
         await user.save()
+         // const token = createJWT(user);
+        // res.json({ token });
         res.json({user})
     }
     catch(err) {
         return res.status(400).json(err);
     }
 }
+
+// function createJWT(user) {
+//   const payload = {
+//     user,
+//   };
+//   const options = {
+//     expiresIn: "24h",
+//   };
+//   return jwt.sign(payload, SECRET, options);
+// }
+
 module.exports = {
     signup
+    //LOGIN
 };
